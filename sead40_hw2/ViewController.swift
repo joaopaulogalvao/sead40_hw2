95641b4953f8df67c68c1c7690c9ecee3475ac58
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-11 08:58:08 -0700

    Add imagePickerControllerDidCancel and show picker animated to camera button.

--- sead40_hw2/ViewController.swift
+++ sead40_hw2/ViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 import Parse
+import QuartzCore
 
 class ViewController: UIViewController {
 
   @IBOutlet weak var imageView: UIImageView!
   
   @IBOutlet weak var btnAlert: UIButton!
+  
+  let picker : UIImagePickerController = UIImagePickerController()
+  
+  let alert = UIAlertController(title: "Button Clicked", message: "Yes the button was clicked", preferredStyle: UIAlertControllerStyle.ActionSheet)
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -25,6 +31,34 @@ class ViewController: UIViewController {
       println("Object has been saved.")
     }
     
+    if UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad {
+      
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (alert) -> Void in
+      println("Alert cancelled")
+    }
+    
+    let camera = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default) { (alert) -> Void in
+      self.presentViewController(self.picker, animated: true, completion: nil)
+      println("Camera selected")
+    }
+    
+    let filter1 = UIAlertAction(title: "Filter1", style: UIAlertActionStyle.Default) { (alert) -> Void in
+      println("Filter 1 selected")
+    }
+    
+    let filter2 = UIAlertAction(title: "Filter2", style: UIAlertActionStyle.Default) { (alert) -> Void in
+      println("Filter 2 selected")
+    }
+    
+    alert.addAction(cancelAction)
+    alert.addAction(camera)
+    alert.addAction(filter1)
+    alert.addAction(filter2)
+    
+    self.picker.delegate = self
+    self.picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
   }
 
   override func didReceiveMemoryWarning() {
@@ -32,6 +66,33 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func showAction(sender: AnyObject) {
+    
+    alert.modalPresentationStyle = UIModalPresentationStyle.PageSheet
+    
+    
+    if let popover = alert.popoverPresentationController {
+      popover.sourceView = view
+      popover.sourceRect = btnAlert.frame
+    }
+    self.presentViewController(alert, animated: true, completion: nil)
+    
+  }
 
+}
+
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+    let image : UIImage = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
+    self.imageView.image = image
+    self.picker.dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    self.picker.dismissViewControllerAnimated(true, completion: nil)
+    println("Picker Cancelled")
+  }
+  
 }
 
