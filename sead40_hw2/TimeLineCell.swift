8bec748b6b11b9e3b961cf01d934d1f70ee67796
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-12 11:13:08 -0700

    Add UIImageView to tableViewCell. Add TimeLineCell class add constraints to cell.

---
+++ sead40_hw2/TimeLineCell.swift
@@ -0,0 +1,14 @@
+//
+//  TimeLineCell.swift
+//  sead40_hw2
+//
+//  Created by Joao Paulo Galvao Alves on 8/12/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineCell: UITableViewCell {
+
+    
+}
