1dccc1e34ead1ebe5d6f956959b38dbff7a70af3
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-12 10:52:24 -0700

    Add TimeLineViewController class.

---
+++ sead40_hw2/TimeLineViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TimeLineViewController.swift
+//  sead40_hw2
+//
+//  Created by Joao Paulo Galvao Alves on 8/12/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineViewController: UIViewController {
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+
+  
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
